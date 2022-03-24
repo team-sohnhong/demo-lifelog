8ae1b595d0e9d07babda513ba93fb5f9047fe4e9
son1473 <57697373+son1473@users.noreply.github.com>
2022-03-24 09:51:23 +0900

    Fix _id to id problem

--- src/schemas/blogpost.schema.ts
+++ src/schemas/blogpost.schema.ts
@@ -3,19 +3,36 @@ import { Document } from 'mongoose';
 
 export type BlogpostDocument = Blogpost & Document;
 
-@Schema()
+@Schema({
+//   id: true,
+  toJSON: {
+    virtuals: true,
+    // transform: function (doc: any, ret: any) {
+    //   delete ret._id;
+    //   delete ret.__v;
+    //   return ret;
+    // },
+  },
+})
 export class Blogpost {
-    @Prop()
-    id: string;
+//   @Prop()
+//   id: string;
 
-    @Prop()
-    owner: string;
+  @Prop()
+  owner: string;
 
-    @Prop()
-    title: string;
+  @Prop()
+  title: string;
 
-    @Prop()
-    content: string;
+  @Prop()
+  content: string;
 }
 
-export const BlogpostSchema = SchemaFactory.createForClass(Blogpost);
+const BlogpostSchema = SchemaFactory.createForClass(Blogpost);
+
+
+BlogpostSchema.virtual('id').get(function (this: BlogpostDocument) {
+  return this._id;
+});
+
+export { BlogpostSchema };
