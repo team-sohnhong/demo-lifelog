c72a7678b3329fdfba196a88720828fb28ac31e6
skytower144 <skytower144@daum.net>
2022-04-05 01:17:29 +0900

    Blogpost schema delete id (remove comment)
    Blogpost schema delete id
    (remove comment)

--- src/schemas/blogpost.schema.ts
+++ src/schemas/blogpost.schema.ts
@@ -4,19 +4,11 @@ import { Document } from 'mongoose';
 export type BlogpostDocument = Blogpost & Document;
 
 @Schema({
-//   id: true,
   toJSON: {
     virtuals: true,
-    // transform: function (doc: any, ret: any) {
-    //   delete ret._id;
-    //   delete ret.__v;
-    //   return ret;
-    // },
   },
 })
 export class Blogpost {
-//   @Prop()
-//   id: string;
 
   @Prop()
   owner: string;
@@ -29,7 +21,6 @@ export class Blogpost {
 }
 
 const BlogpostSchema = SchemaFactory.createForClass(Blogpost);
-
 
 BlogpostSchema.virtual('id').get(function (this: BlogpostDocument) {
   return this._id;
